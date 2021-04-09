6e5ee00c03d75582e4fa47966ac62686d244f5b8
William <wtbeaven@gmail.com>
2021-04-09 16:10:25 +1000

    Made demo level

---
+++ GameJamDriveHome/GamJamDriveHome/Assets/Scripts/Damage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public bool playerHit = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("Tree"))
+        {
+            playerHit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerHit == true)
+        {
+
+        }
+    }
+}
